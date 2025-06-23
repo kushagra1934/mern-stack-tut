7358e8dca45fb51b964484550b0a771f8fd6ff8a
Kushagra Saxena <kushagra.2022ug1093@iiitranchi.ac.in>
2025-06-23 22:19:42 +0530

    feat: add note management features with CRUD operations and enhance UI components-completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailsPage from "./pages/NoteDetailsPage";
@@ -8,6 +8,10 @@ import "./index.css";
 const App = () => {
   return (
     <div data-theme="coffee">
+      <div class="relative h-full w-full bg-slate-950">
+        <div class="absolute bottom-0 left-[-20%] right-0 top-[-20%] h-[800px] w-[800px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        <div class="absolute bottom-0 right-[-20%] top-[200px] h-[800px] w-[800px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+      </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar.jsx";
 import { useState } from "react";
-import RateLimitedUI from "../components/RateLimitedUI";
+import RateLimitedUI from "../components/RateLimitedUI.jsx";
 import { useEffect } from "react";
-import axios from "axios";
+import api from "../lib/axios.js";
 import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard.jsx";
+import NotesNotFound from "../components/NotesNotFound.jsx";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -14,32 +16,43 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:3000/api/notes");
+        const res = await api.get("/notes");
         console.log(res.data);
-        setNotes(res.data);
+        setNotes(res.data.notes);
         setIsRateLimited(false);
       } catch (error) {
         console.log("Error fetching notes");
-        console.log(error)
-        if(error.response?.status==429){
-            setIsRateLimited(true);
+        console.log(error.response);
+        if (error.response?.status == 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
         }
-        else{
-            toast.error("Failed to load notes")
-        }
-      }
-      finally{
+      } finally {
         setLoading(false);
       }
     };
     fetchNotes(); // <-- Call the function here
   }, []); // <-- Add dependency array
+
   return (
     <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
-      <div className="max-w-7xl mx-auto p-4 py-8">
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
 
+        {!loading && notes.length===0 && !isRateLimited && <NotesNotFound/>}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
