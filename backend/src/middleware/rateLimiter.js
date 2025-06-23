28ed24b4982d66c4daa6d3dee2f31eff7f29448b
Kushagra Saxena <kushagra.2022ug1093@iiitranchi.ac.in>
2025-06-23 14:34:02 +0530

    Initialize frontend with Vite and React setup

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,20 +1,22 @@
-// import ratelimit from "../config/upstash.js";
+import ratelimit from "../config/upstash.js";
 
-// const rateLimiter = async (req, res, next) => {
-//   try {
-//     const { success } = await ratelimit.limit("my-rate-limit");
+const rateLimiter = async (req, res, next) => {
 
-//     if (!success) {
-//       return res.status(429).json({
-//         message: "Too many requests, please try again later",
-//       });
-//     }
+  //rate limiting can be done per user, but here as we dont have auth we dont use it, instead use my-limit-key
+  try {
+    const { success } = await ratelimit.limit("my-limit-key");
 
-//     next();
-//   } catch (error) {
-//     console.log("Rate limit error", error);
-//     next(error);
-//   }
-// };
+    if (!success) {
+      return res
+        .status(429)
+        .json({ message: "Too many request, please try again later." });
+    }
 
-// export default rateLimiter;
+    next();
+  } catch (error) {
+    console.log("Rate limit error", error);
+    next(error);
+  }
+};
+
+export default rateLimiter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js"
 
 dotenv.config();
 
@@ -14,13 +15,14 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter)
 
 
-// our simple custom middleware
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//custom middleware
+// app.use((req,res,next)=>{
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`)
 //   next();
-// });
+// })
 
 app.use("/api/notes", notesRoutes);
 
